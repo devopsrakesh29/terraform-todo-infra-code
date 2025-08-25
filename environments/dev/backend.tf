28a027eb4dca27b1bc20b03cf04d5a381dc52c29
RakeshKSingh29 <singhrakesh2986@gmail.com>
2025-08-26 01:13:50 +0530

    pipeline v5

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -2,8 +2,8 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "backendrak-rg"
     storage_account_name = "rakeshtfstg"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
+    container_name       = "tfstatefile"
+    key                  = "devgha.terraform.tfstate"
 
   }
 }
