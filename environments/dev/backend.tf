cec788954d0be41d9dbedf9da64aad8e2951ec4f
RakeshKSingh29 <singhrakesh2986@gmail.com>
2025-09-05 23:54:08 +0530

    Update backend.tf

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "backendrak-rg"
-    storage_account_name = "rakeshtfstg"
+    storage_account_name = "b35rakeshtfstg"
     container_name       = "tfstatefile"
     key                  = "devgha.terraform.tfstate"
 
